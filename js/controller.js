1ff71506d711054d892c6affcafbf84b52737bba
Alicia Quezada <aqueza2@gmail.com>
2016-05-10 23:30:49 -0500

    place event listener on arrow keys

---
+++ js/controller.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	$(".new-game").on("click", function(){
+		var game = new Game('0000202000000000');
+		game.populateBoard();
+		moveListener();
+	});
+	
+	function moveListener(){
+		$("body").keyup(function(event){
+			if (event.which === 38){
+				console.log("up")
+			} else if (event.which === 40) {
+				console.log("down")
+			}	else if (event.which === 39) {
+				console.log("right")
+			} else if (event.which === 37) {
+				console.log("left")
+			};
+		});
+	};
+
+});
+
--- js/initializer.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$(".new-game").on("click", function(){
-		var game = new Game('0000202000000000');
-		game.populateBoard();
-	});
-
-});
-
