13a6cd0d8e77b48aadeefe86adedd3a2dccc8097
Alicia Quezada <aqueza2@gmail.com>
2016-05-10 23:01:29 -0500

    populate the board with the numbers from the string

--- js/game.js
+++ js/game.js
@@ -1,8 +1,11 @@
 function Game(string){
 	this.string = string;
-	console.log(string)
 }
 
-Game.prototype.toString = function(){
-		
+Game.prototype.populateBoard = function(){
+	var string= this.string;
+	var cells = $(".grid-cell");
+	cells.each(function(index){
+		$(cells[index]).text(string[index]);
+	})
 }
--- js/initializer.js
+++ js/initializer.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$(".new-game").on("click", function(){
 		var game = new Game('0000202000000000')
+		game.populateBoard()
 	});
 
 });
